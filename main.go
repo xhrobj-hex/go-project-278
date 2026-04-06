c9f765b309d9242558422ed5e99fb6462f9cff2f
M <m1@mishe1.ru>
2026-04-06 15:25:36 +0300

    Подключены middleware логирования и recovery - step 1

--- main.go
+++ main.go
@@ -9,8 +9,15 @@ import (
 func main() {
 	router := gin.New()
 
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
 	router.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
+	})
+
+	router.GET("/panic", func(c *gin.Context) {
+		panic("test paic")
 	})
 
 	if err := router.Run(":8080"); err != nil {

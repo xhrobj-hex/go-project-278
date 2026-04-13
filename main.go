af494e18926e4e21c027b76b13e1c5adfd00ad6f
m1 <m1@mishe1.ru>
2026-04-13 06:33:43 -0700

    Подключен Sentry для отлеживания ошибок - step 3

--- main.go
+++ main.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"log"
+	"os"
+	"time"
 
+	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// To initialize Sentry's handler, you need to initialize Sentry itself beforehand
+	if err := sentry.Init(sentry.ClientOptions{}); err != nil { // NOTE: Dsn берется из env SENTRY_DSN // !!!: не забыть прописать ее в Render
+		log.Printf("Sentry initialization failed: %v\n", err)
+	}
+	defer sentry.Flush(2 * time.Second)
+
 	router := setupRouter()
 
-	if err := router.Run(":8080"); err != nil {
+	// NOTE: на Render платформа будет подсовывать порт в env PORT
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	if err := router.Run(":" + port); err != nil {
 		log.Fatal(err)
 	}
 }
@@ -19,9 +35,16 @@ func setupRouter() *gin.Engine {
 
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.Use(sentrygin.New(sentrygin.Options{
+		Repanic: true,
+	}))
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
+	})
+
+	r.GET("/test-sentry", func(c *gin.Context) {
+		panic("test sentry panic")
 	})
 
 	return r
